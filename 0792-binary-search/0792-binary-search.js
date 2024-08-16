9edf2c0c33c8feeba40793acbdf38fc7e7cf9f6a
Jeongsoheeme <98510230+Jeongsoheeme@users.noreply.github.com>
2024-08-16 12:14:43 +0900

    Time: 62 ms (27.04%), Space: 51.8 MB (79.91%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target);
+    let centerIdx = Math.round(nums.length / 2);
+    
+    if (target >= nums[centerIdx]) {
+        for (let i = centerIdx; i < nums.length; i++) {
+            if (nums[i] === target) {
+                return i;
+            }
+        }
+    } else {
+        for (let i = 0; i < centerIdx; i++) {
+            if (nums[i] === target) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
 };
