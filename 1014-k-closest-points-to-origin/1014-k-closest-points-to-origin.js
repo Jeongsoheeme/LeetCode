89770ff993f98b01d9356a42b184f18f942ae583
Jeongsoheeme <98510230+Jeongsoheeme@users.noreply.github.com>
2024-08-24 23:29:24 +0900

    Time: 243 ms (24.85%), Space: 70.3 MB (19.22%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    const res = []
+    const minHeap = new MinPriorityQueue()
+    points.forEach(p => {
+        const distanceFromOrigin = Math.sqrt(p[0] * p[0] + p[1] * p[1])
+        minHeap.enqueue(p, distanceFromOrigin)
+    })
+    while(res.length < k) res.push(minHeap.dequeue().element)
+    return res
+};
